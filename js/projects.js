7f66c1bceb434747bbc06209ebdc2188fcc916e7
Ali Diyab <alidiyab1997@gmail.com>
2017-04-05 22:21:23 +0300

    current-projects repo

---
+++ js/projects.js
@@ -0,0 +1,31 @@
+var URL="https://api.github.com/users/alidiyab/repos?sort=created&per_page=5";
+$(document).ready(function() {
+	bindClickEvent();
+	loadCurrentProj();
+})
+
+function bindClickEvent(event) {
+	$("#currentProjects").click(function(event) {
+		$('.currentPro').toggle();	
+		return loadCurrentProj(URL);
+	})
+}
+function loadCurrentProj(url){
+	$.get(url,function(data){
+		$('.currentPro').html('');
+		data.map(loadItm);
+}
+}
+function loadItm(itm){
+	$('.currentPro').append(`<a href="${itm.html_url}">${itm.name}</a><br>`)
+
+}
+
+
+
+// function navbarColor(){
+// 	$('ul.nav.navbar-nav > li').click(function(){
+// 		// $(this).children
+// 		console.log("aaa");
+// 	})
+// }
