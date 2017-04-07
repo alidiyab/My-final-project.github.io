9c86d62e22fba321d75d3f37779a411da58594fa
Ali Diyab <alidiyab1997@gmail.com>
2017-04-07 17:42:29 +0300

    update

--- js/skills.js
+++ js/skills.js
@@ -2,19 +2,19 @@ $(document).ready(function(){
 
 	var container = $('.container');
 	$('#get').click(function () {
-	$.ajax({
-		type: 'GET',
-		url: 'skills.json',
-		dataType: 'json',
-		success:function(data){
-			$.each(data,function (index, item) {
-				$.each(item,function (key, value) {
-					container.append("<li>Name: "+this['name']+"<br>&emsp;&emsp;Desc: "+this['desc']+"</li><br>")
+		$.ajax({
+			type: 'GET',
+			url: 'skills.json',
+			dataType: 'json',
+			success:function(data){
+				$.each(data,function (index, item) {
+					$.each(item,function (key, value) {
+						container.append("<li>Name: "+this['name']+"<br>&emsp;&emsp;Desc: "+this['desc']+"</li><br>")
+					})
 				})
-			})
-		}
+			}
+		})
 	})
-})
 })
 
 
