5324b80277863f1447fa6ee2344e0483c3929676
Ali Diyab <alidiyab1997@gmail.com>
2017-04-09 11:02:24 +0300

    update

--- js/projects.js
+++ js/projects.js
@@ -4,34 +4,3 @@ $(document).ready(function() {
 	loadCurrentProj();
 })
 
-// function bindClickEvent(event) {
-// 	$("#currentProjects").click(function(event) {
-// 		$('.currentPro').toggle();	
-// 		return loadCurrentProj(URL);
-// 	})
-// }
-
-function bindClickEvent(){
-	document.getElementById('currentProjects').onclick(function(){
-		document.getElementById('row').innerHTML(`<a href="${itm.html_url}">${itm.name}</a><br>`)
-	})
-}
-// function loadCurrentProj(url){
-// 	$.get(url,function(data){
-// 		$('.currentPro').html('');
-// 		data.map(loadItm);
-// }
-// }
-// function loadItm(itm){
-// 	$('.currentPro').append(`<a href="${itm.html_url}">${itm.name}</a><br>`)
-
-// }
-
-
-
-// function navbarColor(){
-// 	$('ul.nav.navbar-nav > li').click(function(){
-// 		// $(this).children
-// 		console.log("aaa");
-// 	})
-// }
