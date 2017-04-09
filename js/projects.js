1be50e5e077f9194a2d96a48903b7e1a503530dd
Ali Diyab <alidiyab1997@gmail.com>
2017-04-09 11:00:16 +0300

    update

--- js/projects.js
+++ js/projects.js
@@ -4,22 +4,28 @@ $(document).ready(function() {
 	loadCurrentProj();
 })
 
-function bindClickEvent(event) {
-	$("#currentProjects").click(function(event) {
-		$('.currentPro').toggle();	
-		return loadCurrentProj(URL);
+// function bindClickEvent(event) {
+// 	$("#currentProjects").click(function(event) {
+// 		$('.currentPro').toggle();	
+// 		return loadCurrentProj(URL);
+// 	})
+// }
+
+function bindClickEvent(){
+	document.getElementById('currentProjects').onclick(function(){
+		document.getElementById('row').innerHTML(`<a href="${itm.html_url}">${itm.name}</a><br>`)
 	})
 }
-function loadCurrentProj(url){
-	$.get(url,function(data){
-		$('.currentPro').html('');
-		data.map(loadItm);
-}
-}
-function loadItm(itm){
-	$('.currentPro').append(`<a href="${itm.html_url}">${itm.name}</a><br>`)
+// function loadCurrentProj(url){
+// 	$.get(url,function(data){
+// 		$('.currentPro').html('');
+// 		data.map(loadItm);
+// }
+// }
+// function loadItm(itm){
+// 	$('.currentPro').append(`<a href="${itm.html_url}">${itm.name}</a><br>`)
 
-}
+// }
 
 
 
--- js/skills.js
+++ js/skills.js
@@ -1,20 +1,37 @@
-$(document).ready(function(){
+// $(document).ready(function(){
 
-	var container = $('.container');
-	$('#get').click(function () {
-		$.ajax({
-			type: 'GET',
-			url: 'skills.json',
-			dataType: 'json',
-			success:function(data){
-				$.each(data,function (index, item) {
-					$.each(item,function (key, value) {
-						container.append("<li>Name: "+this['name']+"<br>&emsp;&emsp;Desc: "+this['desc']+"</li><br>")
-					})
-				})
-			}
-		})
-	})
-})
+// 	var container = $('.container');
+// 	$('#get').click(function () {
+// 		$.ajax({
+// 			type: 'GET',
+// 			url: 'skills.json',
+// 			dataType: 'json',
+// 			success:function(data){
+// 				$.each(data,function (index, item) {
+// 					$.each(item,function (key, value) {
+// 						container.append("<li>Name: "+this['name']+"<br>&emsp;&emsp;Desc: "+this['desc']+"</li><br>")
+// 					})
+// 				})
+// 			}
+// 		})
+// 	})
+// })
 
+function toggleNavPanel (x){
 
+var panel = document.getElementById(x), navarrow = document.getElementById('navarrow'), maxH = "300px";
+ 
+ if(panel.style.height == maxH){
+ 	panel.style.height= "0px";	
+ 	navarrow.innerHTML = "&#9662;";
+ }
+else{
+	panel.style.height = maxH;
+	navarrow.innerHTML = "&#9662;";
+}
+
+}
+
+// function bindToggleEvent(){
+
+// }
